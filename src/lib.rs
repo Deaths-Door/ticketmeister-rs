9f0cb73553d1da01c992f94756f7b0a1f65d9b02
Deaths-Door <aaravaditya51@gmail.com>
2023-07-10 00:09:41 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,38 @@
-#![doc = include_str!("../README.md")]
+use api_request_utils_rs::{
+    RequestInfo,
+    RequestModifiers,
+    RequestDefaults,
+    RequestHandler,
+    RequestError,
+    reqwest::{
+        Client,
+        RequestBuilder,
+    },
+};
+//TODO add discovery feature for this instead
 
-#![forbid(
-    missing_docs,
+pub struct Discovery<'a> {
+    client : Client,
+    api_key : &'a str,
+}
 
-    unsafe_code,
-    
-    unused_imports,
-    unused_variables,
-    unused_mut,
-    unused_results,
-    unused_allocation,
-    unused_must_use,
-    unused_import_braces,
+impl RequestInfo for Discovery<'_> {
+    const BASE_URL : &'static str = " https://app.ticketmaster.com/discovery/v2";
+}
 
-    unreachable_patterns,
+impl RequestModifiers for Discovery<'_> {
+    fn create_endpoint(endpoint: &str) -> String {
+        format!("{}/{}.json",Self::BASE_URL,endpoint)
+    }
+}
 
-    trivial_casts,
+impl RequestDefaults for Discovery<'_> { 
+    fn client(&self) -> &Client {
+        &self.client
+    }
+    fn default_parameters(&self,request_builder: RequestBuilder) -> RequestBuilder {
+        request_builder.query(&[("apikey", self.api_key)])
+    }
+}
 
-    unsafe_op_in_unsafe_fn,
-    
-    overflowing_literals,
-)]
-
-mod structs;
-mod enums;
-
-pub use self::structs::*;
-pub use self::enums::*;
-
-#[cfg(feature = "discovery")]
-mod discovery;
-
-#[cfg(feature = "discovery")]
-pub use self::discovery::*;
+impl RequestHandler for Discovery<'_> {}
